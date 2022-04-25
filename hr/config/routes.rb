6bef563bbc3fe7cddf531f2fc7112fb37f35fcd6
EC2 Default User <ec2-user@ip-172-31-13-36.ec2.internal>
2022-04-25 15:50:31 +0000

    MODIFIED PROJECT FILES

--- hr/config/routes.rb
+++ hr/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :resumes
+  get 'welcome/index'
+ 
+  root 'welcome#index'
+  
+  
+  resources :resumes do
+    resources :skills
+  end
 end
